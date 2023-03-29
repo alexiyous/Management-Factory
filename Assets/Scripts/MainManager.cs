02f15304f4b1cfc0f28d31caccaf9106582a39de
alexiyous <alexiusrenanta@gmail.com>
2023-03-29 23:45:36 +0700

    Applying  Profiling, Optimization, Encapsulised

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -5,7 +5,7 @@ using System.IO;
 
 public class MainManager : MonoBehaviour
 {
-    public static MainManager Instance;
+    public static MainManager Instance { get; private set; } // add getter to the end of the line
 
     public Color TeamColor;
 
